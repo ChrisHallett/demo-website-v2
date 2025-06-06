c97a633869ec53d8f266c4c12cbcccf615693e13
Chris Hallett <chrishallettprogrammer@gmail.com>
2025-06-06 14:03:06 +1000

    Added header styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entreprenurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
     </>
   );
 }
